78f8119297bd0ed74b5017c75771d1271bd26c93
l-freeze <mmmm.yamamoto@gmail.com>
2022-10-03 01:26:41 +0900

    ただのhtmlや文字列を返すサンプル

--- fsharp_snippets/Program.fs
+++ fsharp_snippets/Program.fs
@@ -1,2 +1,45 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿//Install-Package Giraffe
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+IO.Directory.GetCurrentDirectory () |> printfn "%A"
+
+__SOURCE_DIRECTORY__ |> printfn "%A"
+__SOURCE_FILE__ |> printfn "%A"
+__LINE__ |> printfn "%A"
+
+
+let documentRoot = [|__SOURCE_DIRECTORY__; "web"; "public"|] |> IO.Path.Combine 
+documentRoot |> printfn "documentRoot: %s"
+IO.Path.Combine (documentRoot, "index.html") |> printfn "index -> %A"
+
+
+let webApp =
+    choose [
+        route "/ping"   >=> text "pong" //http://localhost:5000/ping
+        route "/"       >=> htmlFile (IO.Path.Combine (documentRoot, "index.html")) ] //http://localhost:5000
+
+let configureApp (app : IApplicationBuilder) =
+    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    // Add Giraffe dependencies
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
